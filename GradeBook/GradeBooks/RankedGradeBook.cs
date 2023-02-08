e81bbbe6b5ee65529c2971c1de94160cb1b5093f
SuprajaKatta9 <supraja.katta@klingelnberg.com>
2023-02-08 16:19:28 +0530

    Add project files.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,54 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public  class RankedGradeBook:BaseGradeBook
+
+    {
+        public RankedGradeBook(string name, bool isWeighted) :base(name, isWeighted)
+        {
+            Type = GradeBookType.Ranked;
+
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count<5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade <80 &averageGrade >=60)
+
+                return 'B';
+            else if (averageGrade <60 && averageGrade >= 40)
+                return 'C';
+            else if (averageGrade <40 && averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+
+            
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
